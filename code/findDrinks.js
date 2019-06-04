050e77723e415bbee693d3da913ae2f50e8d6046
Scott Sperling <scsper@gmail.com>
2019-06-04 14:28:18 -0700

    Share action js code.

--- code/findDrinks.js
+++ code/findDrinks.js
@@ -2,51 +2,10 @@ module.exports.function = function findDrinks (drinkName) {
   drinkName = drinkName.toLowerCase()
   var http = require('http')
   var console = require('console')
+  var mungeDrinks = require('./lib/mungeDrinks')
   var response = http.getUrl('https://www.thecocktaildb.com/api/json/v2/8673533/search.php?s=' + drinkName, {
     format: 'json'
   })
 
-  var drinks = response.drinks
-  
-  return drinks.map(function (drink) {
-    var ingredientsArr = []
-    var measurementsArr = []
-    
-    for (var i = 1; i < 16; i++) {
-      ingredientsArr.push(drink['strIngredient' + i])
-      measurementsArr.push(drink['strMeasure' + i])
-    }
-    
-    ingredientsArr = ingredientsArr.filter(function (ingredient) {
-      return ingredient !== ''
-    })
-    
-    measurementsArr = measurementsArr.filter(function (measurement) {
-      return measurement !== ''
-    })
-    
-    var ingredients = []
-    
-    for (var i = 0; i < ingredientsArr.length; i++) {
-      var fullIngredientDescription = ingredientsArr[i]
-
-      if (measurementsArr[i]) {
-        fullIngredientDescription = measurementsArr[i] + ' ' + ingredientsArr[i]
-      } 
-      
-      ingredients.push({
-        ingredient: ingredientsArr[i],
-        measurement: measurementsArr[i],
-        fullIngredientDescription: fullIngredientDescription
-      })
-    }
-    
-    return {
-      id: drink.idDrink,
-      name: drink.strDrink,
-      instructions: drink.strInstructions,
-      thumbnail: drink.strDrinkThumb,
-      ingredients: ingredients
-    }
-  })
+  return response.drinks.map(mungeDrinks)
 }
---
+++ code/lib/mungeDrinks.js
@@ -0,0 +1,41 @@
+module.exports = function (drink) {
+    var ingredientsArr = []
+    var measurementsArr = []
+    
+    for (var i = 1; i < 16; i++) {
+      ingredientsArr.push(drink['strIngredient' + i])
+      measurementsArr.push(drink['strMeasure' + i])
+    }
+    
+    ingredientsArr = ingredientsArr.filter(function (ingredient) {
+      return ingredient !== ''
+    })
+    
+    measurementsArr = measurementsArr.filter(function (measurement) {
+      return measurement !== ''
+    })
+    
+    var ingredients = []
+    
+    for (var i = 0; i < ingredientsArr.length; i++) {
+      var fullIngredientDescription = ingredientsArr[i]
+
+      if (measurementsArr[i]) {
+        fullIngredientDescription = measurementsArr[i] + ' ' + ingredientsArr[i]
+      } 
+      
+      ingredients.push({
+        ingredient: ingredientsArr[i],
+        measurement: measurementsArr[i],
+        fullIngredientDescription: fullIngredientDescription
+      })
+    }
+    
+    return {
+      id: drink.idDrink,
+      name: drink.strDrink,
+      instructions: drink.strInstructions,
+      thumbnail: drink.strDrinkThumb,
+      ingredients: ingredients
+    }
+  }
