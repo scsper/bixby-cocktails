65b45cb5ed1211fc2b4aab97e48529f7889d907c
Scott Sperling <scsper@gmail.com>
2019-06-04 10:06:00 -0700

    Implement FindDrinks without a view or any testing.

---
+++ code/findDrinks.js
@@ -0,0 +1,40 @@
+module.exports.function = function findDrinks (drinkName) {
+  var http = require('http')
+  var response = http.getUrl('https://www.thecocktaildb.com/api/json/v2/8673533/search.php?s=' + drinkName)
+  var drinks = response.drinks
+  
+  return drinks.map(function (drink) {
+    var ingredientsArr = []
+    var measurementsArr = []
+    
+    for (var i = 1; i < 16; i++) {
+      ingredientsArr.push(drink['strIngredient' + i])
+      measurementsArr.push(drink['strMeasure' + i])
+    }
+    
+    ingredientsArr = ingredientsArr.filter(function (ingredient) {
+      return ingredient !== ''
+    })
+    
+    measurementsArr = measurementsArr.filter(function (measurement) {
+      return measurement !== ''
+    })
+    
+    var ingredients = []
+    
+    for (var i = 0; i < ingredientsArr.length; i++) {
+      ingredients.push({
+        ingredient: ingredientsArr[i],
+        measurement: measurementsArr[i]
+      })
+    }
+    
+    return {
+      id: drink.idDrink,
+      name: drink.strDrink,
+      instructions: drink.strInstructions,
+      thumbnail: drink.strDrinkThumb,
+      ingredients: ingredients
+    }
+  })
+}
